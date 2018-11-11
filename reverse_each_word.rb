aca815843913197c40413de682029c5783b85569
Cody Rickman <cody@creativemindstudios.com>
2018-11-11 02:33:56 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,8 +1,7 @@
 def reverse_each_word (string)
   array = string.split(" ");
   new_array = []
-  array.each do |word|
-    new_array << word.reverse
+  array.collect do |word|
+    word.reverse
   end
-  return new_array
 end
